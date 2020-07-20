a66b6772c8014e8005ab06743fac7cdc52282370
Md. Samshad Rahman <samshad.ru@gmail.com>
2020-07-20 20:02:57 +0600

    collecting_users_answers

--- collect_answers_of_users.py
+++ collect_answers_of_users.py
@@ -6,17 +6,12 @@ import pandas as pd
 from Data.apikey import access_token as apikey
 
 
-def check(id):
-    ls = [file.split('.')[0] for file in os.listdir('Data/user_answers/')]
-    return id in ls
-
-
 def getData(id):
     try:
         print(user, ":", end=" ")
         answers = SITE.fetch(f'users/{user}/answers', filter='withbody')
         print(len(answers['items']))
-        if len(answers['items']) >= 500:
+        if len(answers['items']) > 0:
             with open('Data/user_answers/' + str(user) + '.json', 'w', encoding='utf-8') as outfile:
                 json.dump(answers, outfile, ensure_ascii=False, indent=4)
     except:
@@ -28,9 +23,10 @@ def getData(id):
 SITE = StackAPI('stackoverflow', key=apikey)
 df = pd.read_csv("Data/Stack_Twitter.csv", encoding='unicode_escape')
 users = [userid.split('/')[2] for userid in list(df.STACK_ID)]
+ls = [file.split('.')[0] for file in os.listdir('Data/user_answers/')]
 
 for user in users:
-    if not check(user):
+    if user not in ls:
         print("Collecting Data of user:", user)
         getData(user)
     else:
